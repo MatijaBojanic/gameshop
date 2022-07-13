473a0d4a86952c889df728c7459c9eb539e268dd
MatijaBojanicC <matija.bojanic@codeus.me>
2022-07-13 14:05:54 +0200

    Added change password route

--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
-from rest_framework.permissions import AllowAny
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, ChangePasswordSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.models import User
 from rest_framework import generics
@@ -15,3 +15,9 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
