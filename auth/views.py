c552b6bb1fee4a931090d0f46ecfd40a27b49222
MatijaBojanicC <matija.bojanic@codeus.me>
2022-07-13 13:53:28 +0200

    Added auth app for login and registering. Added gitignore and added editor config for commit cleanup

---
+++ auth/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
+from rest_framework import generics
+
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
--- gameshop/urls.py
+++ gameshop/urls.py
@@ -30,7 +30,5 @@ router.register(r'product', ProductViewSet, basename='Product')
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('auth/', include('auth.urls')),
 ]
