0c0ee7e478719eac203b4bc7b3336a889f28dbc7
MatijaBojanicC <matija.bojanic@codeus.me>
2022-07-30 12:31:36 +0200

    cleanup

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['gameshop-g5.com', 'www.gameshop-g5.com', 'localhost']
 
