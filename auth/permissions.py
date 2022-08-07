c05f0693c37f21b66332ef0a77c97ae985023ce8
MatijaBojanicC <matija.bojanic@codeus.me>
2022-08-07 22:50:05 +0200

    removed unused permission

--- auth/permissions.py
+++ auth/permissions.py
@@ -2,17 +2,6 @@ from rest_framework import permissions
 from shop_api.models import Order
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
-    """
-    Admin should be able to do unsafe methods on objs, while regular users should be able to do safe stuff
-    """
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return False
-
-
 class IsSuperuser(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_superuser
